28c1038c805efcca4fa7ef53a6088502681a3b6f
ChanningTong <Channing_TongCN@outlook.com>
2023-06-26 14:52:55 +0800

    version 0.0.8

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/***
+ * @Author: ChanningTong Channing_TongCN@outlook.com
+ * @Date: 2023-06-11 22:10:24
+ * @LastEditors: ChanningTong Channing_TongCN@outlook.com
+ * @LastEditTime: 2023-06-17 15:16:55
+ * @FilePath: \BasicGIS\main.cpp
+ * @Description:
+ * @
+ * @Copyright (c) 2023 by ChanningTong, All Rights Reserved.
+ */
 // @主程序，只负责控制流程
 #include "Solution.h"
 #include "Objects.h"
@@ -6,7 +16,6 @@
 Commander cmder;
 int main(int argc,char *argv[]) {
 	Initialize(cmder);
-    bool bExit = false;
 	while (!cmder.getCommand()) {}
 	ShutDown(cmder);
 	return 0;
